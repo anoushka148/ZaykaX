e51c9f26c88abfb896db00995cb8913695624dbd
anoushka148 <118325807+anoushka148@users.noreply.github.com>
2024-06-18 00:58:47 +0530

    Add files via upload

---
+++ src/screens/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
+import Card from '../components/Card'
+import Carousel from '../components/Carousel'
+
+export default function Home() {
+  return (
+    <div>
+      <div><NavBar /></div>
+      <div><Carousel /></div>
+      <div><Card/></div>
+
+      <div><Footer></Footer></div>
+    </div>
+  )
+}
